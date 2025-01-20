f2ac095c65e029f087af2472719c5e5a27153f3a
Hamayun Safdar Gill <92295289+hamayun-safdar@users.noreply.github.com>
2025-01-20 13:00:02 +0500

    jelly fish demo app

---
+++ src/components/JsonViewer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const JsonViewer = ({ content }) => {
+  return (
+    <div className="jsonViewer">
+      <pre> {content}</pre>
+    </div>
+  );
+};
+
+export default JsonViewer;
